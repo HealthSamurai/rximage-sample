6f5430e87bb0a95fd28ec3b100b154571514487b
Vyacheslav Mikushev <vyacheslav.mik.8@gmail.com>
2017-09-27 12:46:33 +0300

    Remove logs

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ function searchMedicationStatements ($http, scope, opts) {
 	url: 'https://open-ic.epic.com/FHIR/' + opts.fhirServiceUrl + '/MedicationStatement?patient=' + opts.patientId
     }).then(function(resp) {
 	scope.medications = resp.data.entry.filter((x)=>{return x.resource.resourceType == "MedicationStatement"}).map((x)=>{return x.resource;});
-	console.log(scope.medications);
     }, printError);
 }
 
